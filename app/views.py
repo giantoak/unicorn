ed90b7b0cf9bd426179e5853876cfc9679303c20
Ubuntu <ubuntu@ip-10-181-78-224.ec2.internal>
2015-01-23 18:26:23 +0000

    transitioned to flask

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
-from app import app
-
+from app import app, es
 from flask import jsonify
 from flask import render_template
 from flask import url_for
@@ -10,6 +9,32 @@ from flask import session
 from werkzeug import secure_filename
 import flask
 import tempfile
+import json
+
+from elasticsearch_dsl import Search, Q
+
+@app.route('/search/<query>')
+def search_endpoint(query):
+    q = {
+            "query" : {
+                "match" : {
+                    "file" : query
+                    }
+                },
+
+            "highlight": { "fields": { "file": { } } }
+            }
+    raw_response = es.search(body=q, index="dossiers", df="file")
+    clean_response = []
+
+    for resp in raw_response['hits']['hits']:
+        clean_response.append((resp['_source']['title'], resp['highlight']['file'][0]))
+
+    return Response(json.dumps({'result': clean_response}), mimetype='text/json')
+
+@app.route('/')
+def root():
+    return render_template('home-tabview.html')
 
 @app.route('/upload/')
 def upload_form():
