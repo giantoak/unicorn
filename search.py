cd897f0796903027d3fde5d57a1c3816154e131e
giantoak <graham.mueller@giantoak.com>
2015-02-02 13:14:35 -0500

    highlighting added

--- search.py
+++ search.py
@@ -8,11 +8,20 @@ import requests
 
 
 def run(term=""):
-	url=''+term
-	r=requests.get(url)
+	url='http://ec2-54-145-248-41.compute-1.amazonaws.com:9200/dossiers/_search'
+	body={
+		"query":{
+			"match":{
+				"file":term
+			}
+		},
+		"highlight":{"fields":{"file":{}}}
+
+	}
+	r=requests.post(url,json.dumps(body))
 	data=r.json()
 	titles=[]
 	for hit in data['hits']['hits']:
-		titles.append(hit['_source']['title'])
+		titles.append({"doc":hit['_source']['title'],"snippet":hit['highlight']['file']})
 
 	return json.dumps(titles)
