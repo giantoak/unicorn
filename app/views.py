b13374905c41fa041632e7dffb28a99f7b3db967
Ubuntu <ubuntu@ip-10-181-78-224.ec2.internal>
2015-03-11 19:42:14 +0000

    reskin + download files

--- app/views.py
+++ app/views.py
@@ -6,14 +6,40 @@ from flask import redirect
 from flask import Response
 from flask import request
 from flask import session
+from flask import abort
+from flask import send_file
 from werkzeug import secure_filename
 import flask
 import tempfile
 import json
 
 from elasticsearch_dsl import Search, Q
+import io
 
 DEFAULT_INDEX = 'dossiers'
+
+@app.route('/download/<doc_id>')
+def download_endpoint(doc_id):
+    q = {
+            "query" : {
+                "match" : {
+                    "_id" : doc_id
+                    }
+                },
+            }
+    response = es.search(body=q, index=DEFAULT_INDEX)
+    
+    try:
+        f = io.BytesIO(response['hits']['hits'][0]['_source']['file']
+                .decode('base64'))
+        fn = response['hits']['hits'][0]['_source']['title']
+    except KeyError, IndexError:
+        abort(404)
+
+    return send_file(f,
+            as_attachment=True,
+            attachment_filename=fn)
+
 
 @app.route('/search/<query>')
 def search_endpoint(query):
@@ -30,7 +56,8 @@ def search_endpoint(query):
     clean_response = []
 
     for resp in raw_response['hits']['hits']:
-        clean_response.append((resp['_source']['title'], resp['highlight']['file'][0]))
+        clean_response.append((resp['_id'], resp['_source']['title'], 
+            resp['highlight']['file'][0]))
 
     return Response(json.dumps({'result': clean_response}), mimetype='text/json')
 
