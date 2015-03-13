33592de9e9b258c9b66982d56ec54a74f1baf180
Ubuntu <ubuntu@ip-10-181-78-224.ec2.internal>
2015-03-13 17:17:11 +0000

    templates + config file + requirements

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # unicorn
 centralized search for wildlife trafficking
+
+# Installation
+
+Remove ".template" from runconfig.py and app/config.py.
+
+Configure port and elasticsearch endpoint.
+
+```$virtualenv ve
+$source ve/bin/activate
+$pip install -r requirements.txt```
+
--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 import random
 from elasticsearch import Elasticsearch
+from config import es_url
 
 app = Flask(__name__)
 app.secret_key = str(random.SystemRandom().random())
 
-es = Elasticsearch()
+es = Elasticsearch(es_url)
 
 from app import views
