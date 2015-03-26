6a89bc2aa4a9d0bf051c88e80faac48b43d02741
samzhang111 <sam.zhang@giantoak.com>
2015-03-26 20:58:38 +0000

    ENH: Optional SSL support in debug mode
    - Updated run.py with SSL support for key/crt named 'unicorn.key/crt'
    - Updated install.sh and requirements.txt with prequisites
    - Updated runconfig.py.template with cert_dir setting

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-# Install NLTK dependencies
-python nltk_deps.py
+# Install pip, virtualenv, setup python environment
+apt-get install python-pip
+pip install virtualenv
+virtualenv env
 
 # Install unoconv
 apt-get install -y unoconv
 
+# Install packages for SSL
+apt-get install -y libssl-dev libffi-dev
+
+# Install Python dependencies
+source env/bin/activate
+pip install -r requirements.txt
+
+# Install NLTK dependencies
+python nltk_deps.py
+
 # Initialize unoconv
-unoconv -l
+unoconv -l &
--- run.py
+++ run.py
@@ -1,5 +1,20 @@
 from app import app
 from runconfig import port
+import os
+try:
+    from runconfig import cert_dir
+    from OpenSSL import SSL
+except ImportError:
+    cert_dir = None
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=port)
+    if cert_dir:
+        context = SSL.Context(SSL.TLSv1_2_METHOD)
+        context.use_privatekey_file(os.path.join(cert_dir, 'unicorn.key'))
+        context.use_certificate_file(os.path.join(cert_dir, 'unicorn.crt'))
+        app.run(debug=True, host='0.0.0.0', port=port, ssl_context=context)
+
+    else:
+        app.run(debug=True, host='0.0.0.0', port=port)
+
