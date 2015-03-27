b9ba628df9767e2bf397363d33971df5278c4ce8
giantoak <graham.mueller@giantoak.com>
2015-03-27 10:08:00 -0400

    ENH: added entity network / wc

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import random
 from elasticsearch import Elasticsearch
-from config import es_url, admin_username, admin_password
+from config import es_url, es_port, admin_username, admin_password
 from flask.ext.basicauth import BasicAuth
 
 app = Flask(__name__)
@@ -12,6 +12,6 @@ app.config['BASIC_AUTH_PASSWORD'] = admin_password
 app.config['BASIC_AUTH_FORCE'] = True
 
 basic_auth = BasicAuth(app)
-es = Elasticsearch(es_url)
+es = Elasticsearch(es_url, port=es_port)
 
 from app import views
