ccdd8fec8011d5cdcafc1d4e7f4ad241451e5bec
samzhang111 <sam.zhang@giantoak.com>
2015-03-28 00:01:02 +0000

    MAINT: Removed Flask-Auth from app and requirements

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 import random
 from elasticsearch import Elasticsearch
 from config import es_url, es_port, admin_username, admin_password, db_conn_str
-from flask.ext.basicauth import BasicAuth
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.login import LoginManager
@@ -10,9 +9,6 @@ from flask.ext.login import LoginManager
 app = Flask(__name__)
 app.secret_key = str(random.SystemRandom().random())
 
-app.config['BASIC_AUTH_USERNAME'] = admin_username
-app.config['BASIC_AUTH_PASSWORD'] = admin_password
-app.config['BASIC_AUTH_FORCE'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = db_conn_str
 
 db = SQLAlchemy(app)
@@ -20,7 +16,6 @@ flask_bcrypt = Bcrypt(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-basic_auth = BasicAuth(app)
 es = Elasticsearch(es_url, port=es_port)
 
 from app.models import *
