28faebf1a431d9f4fc286edddc56c1c2eec6279f
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-02-22 22:44:41 -0800

    Removed elasticdump requirement, added scikit-learn to requirements.txt, fixed install order.

--- install.sh
+++ install.sh
@@ -1,47 +1,46 @@
-#!/bin/sh
+#!/bin/bash
 
-# Install nodejs, npm, and elasticdump
+# Basic update
+sudo apt-get -y autoremove
 sudo apt-get -y update
-sudo apt-get -y install nodejs
-sudo apt-get -y install npm
-sudo npm install elasticdump -g
 
-# create a symlink for nodejs
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+# get git
+sudo apt-get -y install git
 
-# Install pip, virtualenv, setup python environment
-sudo apt-get -y install python-pip
-# sudo pip install virtualenv
-# virtualenv env
-# source env/bin/activate
+# Install scikit learn and python dependencies
+sudo apt-get -y install python-pip build-essential python-dev python-setuptools python-numpy python-scipy libatlas-dev libatlas3gf-base
 
-# Install sci-kit learn dependencies
-sudo apt-get -y install build-essential python-dev python-setuptools \
-                     python-numpy python-scipy \
-                     libatlas-dev libatlas3gf-base
+# Update math libraries
+sudo update-alternatives --set libblas.so.3  /usr/lib/atlas-base/atlas/libblas.so.3
+sudo update-alternatives --set liblapack.so.3 /usr/lib/atlas-base/atlas/liblapack.so.3
 
-# Install sci-kit learn
-sudo pip install scikit-learn
+# Install postgresql for security DB
+sudo apt-get -y install postgresql postgresql-contrib python-psycopg2 libpq-dev
 
-# Install postgresql and psycopg2
-sudo apt-get -y install postgresql postgresql-contrib
-sudo apt-get -y install python-psycopg2
-sudo apt-get -y install libpq-dev
-
-# Install unoconv
-sudo apt-get install -y unoconv
+# Install mysql for geodict.
+sudo apt-get -y install mysql-server mysql-client libmysqlclient-dev
 
 # Install packages for SSL
-sudo apt-get install -y libssl-dev libffi-dev
+sudo apt-get -y install libssl-dev libffi-dev
 
-# Install psycopg dependencies
-apt-get install libpq-dev
-
-# Install Python dependencies
+# Install necessary python modules
 sudo pip install -r requirements.txt
 
 # Install NLTK dependencies
 python nltk_deps.py
+
+# Install unoconv
+sudo apt-get -y install unoconv
+
+# Clone the geodict library into the local dir.
+# It isn't a module
+git clone https://github.com/giantoak/geodict geodict
+
+# Start MySQL if it's off and populate with geodict data
+sudo service mysql start
+cd geodict
+python populate_database.py
+cd ..
 
 # Initialize unoconv
 unoconv -l &
@@ -56,7 +55,9 @@ sudo service elasticsearch stop
 sudo /usr/share/elasticsearch/bin/plugin --install elasticsearch/elasticsearch-mapper-attachments/2.5.0
 
 # install the elasticsearch carrot2 plugin
-sudo /usr/share/elasticsearch/bin/plugin --install org.carrot2/elasticsearch-carrot2/1.9.0
+# sudo /usr/share/elasticsearch/bin/plugin --install org.carrot2/elasticsearch-carrot2/1.9.0
+# According to github.com/carrot2/elasticsearch-carrot2, for ES 1.5.2 we should be using v. 1.8.0 of the carrot 2 plugin
+sudo /usr/share/elasticsearch/bin/plugin --install org.carrot2/elasticsearch-carrot2/1.8.0
 
 # Reboot elasticsearch as a service
 sudo service elasticsearch start
@@ -65,39 +66,26 @@ sudo service elasticsearch start
 sleep 10
 
 # delete any index call dossiers then recreate it (wipe it)
-curl -XDELETE "http://localhost:9200/dossiers/"  ; curl -XPUT "http://localhost:9200/dossiers/" 
+curl -XDELETE "http://localhost:9200/dossiers/"
+curl -XPUT "http://localhost:9200/dossiers/"
 
 # check the index size (should be 0 documents)
-curl 'localhost:9200/_cat/indices?v'
+curl "localhost:9200/_cat/indices?v"
 
-# use elasticdump to build our index. First, read in the mapping, then read in the data
-sudo elasticdump \
-  --input=dossiers_mapping.json \
-  --output=http://localhost:9200/dossiers \
-  --type=mapping 
+# use curl to build our index. First, read in the mapping, then read in the data
+curl -XPUT "http://localhost:9200/dossiers/_mapping/attachment" -d @dossiers_mapping.json
 
 # print the mapping and make sure it looks like a large JSON object
 curl -XGET 'http://localhost:9200/dossiers/_mapping/attachment?pretty'
 
 # finally, load some dummy data
-sudo elasticdump \
-  --bulk=true \
-  --input=dossiers.json \
-  --output=http://localhost:9200/dossiers \
-  --type=data
+curl -XPUT "http://localhost:9200/dossiers/_bulk" --data-binary @dossiers.json
 
 bash db_setup.sh
 # python createdb.py
 
-python run.py
-
 ###### uncomment the below if you do not want to initialize with any sample data! ####
-# curl -XDELETE "http://localhost:9200/dossiers/" ; curl -XPUT "http://localhost:9200/dossiers/" 
+# curl -XDELETE "http://localhost:9200/dossiers/" ;
+# curl -XPUT "http://localhost:9200/dossiers/"
 
-#sudo elasticdump \
-#  --bulk=true \
-#  --input=dossiers_mapping.json \
-#  --output=http://localhost:9200/dossiers \
-#  --type=mapping \
-#  --debug=true
-
+python run.py
