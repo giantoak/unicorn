536ba3b2efd29f8dcdd4072beba255bcf91bf04b
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-03-01 20:00:18 -0800

    PEP-8 changes

--- createdb.py
+++ createdb.py
@@ -8,12 +8,10 @@ focus = Organization('Focus Africa', 'focus-africa.co.za')
 admin_group = Organization('admins')
 
 admin_pw_hash = flask_bcrypt.generate_password_hash(admin_password)
-admin = User(admin_username, admin_pw_hash, organization=admin_group, 
-        moderator=True,
-        admin=True)
+admin = User(admin_username, admin_pw_hash, organization=admin_group,
+             moderator=True, admin=True)
 
 db.session.add(focus)
 db.session.add(admin_group)
 db.session.add(admin)
 db.session.commit()
-
