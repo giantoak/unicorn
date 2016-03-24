8a12aab11b61ee1dddf1ddd9c62620a760d787ea
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-03-24 12:50:39 -0700

    Finishing switch to config.py

--- app/config.py
+++ app/config.py
@@ -1,13 +1,15 @@
+# Elasticsearch configuration
 es_url = 'localhost'
 es_port = 9200
 es_index = 'dossiers'
 
+# Postgres configuration
+db_conn_str = 'postgresql://unicorn:unicorn@127.0.0.1:5432/'
+
+# Unicorn confi
 admin_username = 'admin'
 admin_password = 'admin'
 tmp_dir = '/tmp/'
-
-db_conn_str = 'postgresql://unicorn:unicorn@127.0.0.1:5432/<db>'
-
 uni_host = '0.0.0.0'
 uni_port = 5000
 # Optional: the directory that contains "unicorn.key" and "unicorn.cert"
