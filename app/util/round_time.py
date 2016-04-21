9eb18c92579f71b4883998d6b67e0e9137a3c67d
Pete[r] M. Landwehr <pmlandwehr@gmail.com>
2016-04-21 01:33:16 -0700

    Cleaned up docstrings and imports

--- app/util/round_time.py
+++ app/util/round_time.py
@@ -1,27 +1,36 @@
-import datetime
+from datetime import datetime
+from datetime import timedelta
 
 
 def round_time(dt=None, round_to=60):
+    """
+
+    :param datetime.datetime dt:
+    :param int round_to:
+    :return datetimte.datetime:
+    """
     if dt is None:
-        dt = datetime.datetime.now()
+        dt = datetime.now()
     seconds = (dt - dt.min).seconds
     # // is a floor division, not a comment on following line:
     rounding = (seconds+round_to/2) // round_to * round_to
-    return dt + datetime.timedelta(0, rounding-seconds, -dt.microsecond)
+    return dt + timedelta(0, rounding-seconds, -dt.microsecond)
 
 
 def round_month_up(origin_date):
     """
     Takes a datetime object and rounds up to the nearest month.
     E.g. 2/14/2013 becomes 3/1/2013
+    :param datetime.datetime origin_date:
+    :return datetime.datetime:
     """
     day = origin_date.day
     month = origin_date.month
     year = origin_date.year
     if origin_date.month == 12:
-       delta = datetime.datetime(year + 1, 1, day) - origin_date
+       delta = datetime(year + 1, 1, day) - origin_date
     else:
-       delta = datetime.datetime(year, month + 1, day) - origin_date
+       delta = datetime(year, month + 1, day) - origin_date
     return origin_date + delta - datetime.timedelta(days=day - 1)
 
 
@@ -29,14 +38,19 @@ def round_month_down(origin_date):
     """
     Takes a datetime object and rounds down to the nearest month.
     E.g. 2/14/2013 becomes 2/1/2013
+    :param datetime.datetime origin_date:
+    :return datetime.datetime:
     """
-    down_time = origin_date - datetime.timedelta(days=origin_date.day - 1)
+    down_time = origin_date - timedelta(days=origin_date.day - 1)
     return down_time
 
 
 def week_delta(start, stop):
     """
-    Takes two datettime objects and returns number of weeks between them
+    Takes two datetime objects and returns number of weeks between them
+    :param datetime.datetime start:
+    :param datetime.datetime stop:
+    :return int:
     """
     delta = (stop - start) / 7
     return delta.days
